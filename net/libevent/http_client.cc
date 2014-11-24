13797b34add0f0a98b8255582f7e58d31618bd02
sqfasd <sqf1225@foxmail.com>
2014-11-24 19:28:54 +0800

    test retrie libevent http request after failed

--- net/libevent/http_client.cc
+++ net/libevent/http_client.cc
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <errno.h>
@@ -11,11 +11,22 @@
 
 static struct event_base *base;
 
+struct Client {
+  char* host;
+  int port;
+  char* uri;
+  int id;
+};
+
+Client* client;
+
+void request(Client* client);
+
 static void http_request_done(struct evhttp_request *req, void *ctx) {
 	char buffer[256];
 	int nread;
 
-  if (req == NULL) {
+  if (req == NULL || evhttp_request_get_response_code(req) == 0) {
     /* If req is NULL, it means an error occursurred, but
      *  * sadly we are mostly left guessing what the error
      *   * mainight have been.  We'll do our best... */
@@ -23,6 +34,9 @@ static void http_request_done(struct evhttp_request *req, void *ctx) {
     fprintf(stdout, "socket error = %s (%d)\n",
             evutil_socket_error_to_string(errcode),
             errcode);
+
+    sleep(5);
+    request(client);
     return;
   }
 
@@ -47,16 +61,17 @@ void http_chunk_cb(struct evhttp_request* req, void* arg) {
 	}
 }
 
-void request(char* host, int port, char* uri, int i) {
+void request(Client* client) {
+  printf("request()\n");
   // TODO error checking
   struct bufferevent *bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
-  struct evhttp_connection* evcon = evhttp_connection_base_new(base, NULL, host, port);
-  struct evhttp_request *req = evhttp_request_new(http_request_done, NULL);
+  struct evhttp_connection* evcon = evhttp_connection_base_new(base, NULL, client->host, client->port);
+  struct evhttp_request *req = evhttp_request_new(http_request_done, client);
   evhttp_request_set_chunked_cb(req, http_chunk_cb);
   struct evkeyvalq* output_headers = evhttp_request_get_output_headers(req);
-  evhttp_add_header(output_headers, "Host", host);
+  evhttp_add_header(output_headers, "Host", client->host);
   evhttp_add_header(output_headers, "Connection", "close");
-  int ret = evhttp_make_request(evcon, req, EVHTTP_REQ_GET, uri);
+  int ret = evhttp_make_request(evcon, req, EVHTTP_REQ_GET, client->uri);
   if (ret != 0) {
     fprintf(stderr, "evhttp_make_request() failed\n");
   }
@@ -70,12 +85,13 @@ int main(int argc, char* argv[]) {
   base = event_base_new();
   assert(base);
 
-  char* host = argv[1];
-  int port = atoi(argv[2]);
-  char* uri = argv[3];
-  int count = atoi(argv[4]);
+  client = new Client();
+  client->host = argv[1];
+  client->port = atoi(argv[2]);
+  client->uri = argv[3];
+  const int count = atoi(argv[4]);
   for (int i = 0; i < count; ++i) {
-    request(host, port, uri, i);
+    request(client);
   }
 
   event_base_dispatch(base);
