fdac4c31c1da700ed77390414dfa8d4b7fce1452
sqfasd <sqf1225@foxmail.com>
2015-05-03 19:58:25 +0800

    fix compile warnings & remove unused codes

--- client/beanstalk/get.cc
+++ client/beanstalk/get.cc
@@ -4,27 +4,24 @@
 
 int main(int argc, char* argv[]) {
   BSJ *job;
-  int socket = bs_connect("127.0.0.1", 11300);
+  int socket = bs_connect((char*)"127.0.0.1", 11300);
 
   assert(socket != BS_STATUS_FAIL);
-  assert(bs_use(socket,    "test")    == BS_STATUS_OK);
-  assert(bs_watch(socket,  "test")    == BS_STATUS_OK);
-  assert(bs_ignore(socket, "default") == BS_STATUS_OK);
+  assert(bs_watch(socket,  (char*)"test")    == BS_STATUS_OK);
+  assert(bs_ignore(socket, (char*)"default") == BS_STATUS_OK);
 
   int ret = bs_reserve_with_timeout(socket, 2, &job);
   if (ret != BS_STATUS_OK) {
     printf("bs_reserve failed with code: %d\n", ret);
   } else {
     assert(job);
-    printf("reserve job id: %d size: %lu\n", job->id, job->size);
+    printf("reserve job id: %ld size: %lu\n", job->id, job->size);
     write(fileno(stderr), job->data, job->size);
     write(fileno(stderr), "\r\n", 2);
 
-    printf("delete job id: %d\n", job->id);
+    printf("delete job id: %ld\n", job->id);
     assert(bs_delete(socket, job->id) == BS_STATUS_OK);
     bs_free_job(job);
   }
-
-
   bs_disconnect(socket);
 }
--- client/beanstalk/put.cc
+++ client/beanstalk/put.cc
@@ -1,19 +1,22 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <assert.h>
 #include ".deps/beanstalk-client/beanstalk.h"
 
 int main(int argc, char* argv[]) {
-  int id, socket = bs_connect("127.0.0.1", 11300);
+  int id, socket = bs_connect((char*)"127.0.0.1", 11300);
 
   assert(socket != BS_STATUS_FAIL);
-  assert(bs_use(socket,    "test")    == BS_STATUS_OK);
-  assert(bs_watch(socket,  "test")    == BS_STATUS_OK);
-  assert(bs_ignore(socket, "default") == BS_STATUS_OK);
+  assert(bs_use(socket,    (char*)"test")    == BS_STATUS_OK);
 
+  // sleep(3);
+  printf("before bs_put\n");
   id = bs_put(socket, 0, 0, 60, argv[1], strlen(argv[1]));
 
-  assert(id > 0);
-  printf("put job id: %d\n", id);
-
+  if (id <= 0) {
+    printf("bs_put failed with code: %d\n", id);
+  } else {
+    printf("put job id: %d\n", id);
+  }
   bs_disconnect(socket);
 }
