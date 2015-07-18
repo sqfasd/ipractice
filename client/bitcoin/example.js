4f73c6aa8f9cc7332b5f7f52206bb96da20b73ac
sqfasd <sqf1225@foxmail.com>
2015-07-19 01:32:25 +0800

    add bitcoin clinet example

---
+++ client/bitcoin/example.js
@@ -0,0 +1,34 @@
+var bitcoin = require('bitcoin');
+var assert = require('assert');
+
+var client = new bitcoin.Client({
+  host: 'localhost',
+  port: 9322,
+  user: 'infinitecoinrpc',
+  pass: 'Xp7GijrDdRW6sw5LSSiXnDsoE7fTHic9aLAGXYjJZJL',
+  timeout: 3000
+});
+
+client.getBalance('*', 6, function(err, balance, resHeaders) {
+  if (err) return console.log(err);
+  console.log('balance:', balance);
+});
+
+client.getDifficulty(function(err, difficulty) {
+  assert.ifError(err);
+  assert.ok(typeof difficulty === 'number');
+  console.log('difficulty: ', difficulty);
+});
+
+//client.cmd('help', function(err, commandList) {
+//  assert.ifError(err);
+//  console.log('commandList', commandList);
+//});
+
+client.walletPassphrase('shanqing1', 100, function(err) {
+  if (err) {
+    console.log('walletPassphrase error', err);
+    return;
+  }
+  console.log('walletPassphrase success');
+});
