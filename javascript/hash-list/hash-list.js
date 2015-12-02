bf55da579b411246f09c61a9b970c09323ff3118
sqfasd <sqf1225@foxmail.com>
2015-12-02 20:37:31 +0800

    add test for hash-list & add jshintrc

--- javascript/hash-list/hash-list.js
+++ javascript/hash-list/hash-list.js
@@ -4,65 +4,45 @@ function ListNode(data) {
   this.data = data;
 }
 
-function HashList(compare) {
-  this.head = null;
-  this.tail = null;
-  this.map = {};
-  this.compare = compare;
+function HashList() {
+  this.head_ = null;
+  this.tail_ = null;
+  this.size_ = 0;
+  this.map_ = {};
 }
+
+HashList.prototype.size = function() {
+  return this.size_;
+};
 
 HashList.prototype.add = function(key, value) {
   var newNode = new ListNode(value);
-  this.map[key] = newNode;
-  if (this.head == null) {
-    this.head = this.tail = newNode;
-  } else if (this.head == this.tail) {
-    if (this.compare(value, this.head.data)) {
-      this.head.prev = newNode;
-      newNode.next = this.head;
-      this.head = newNode;
-    } else {
-      this.head.next = newNode;
-      newNode.prev = this.head;
-      this.tail = newNode;
-    }
+  this.map_[key] = newNode;
+  if (this.head_ === null) {
+    this.head_ = this.tail_ = newNode;
+  } else if (this.head_ === this.tail_) {
+    this.head_.next = newNode;
+    newNode.prev = this.head_;
+    this.tail_ = newNode;
   } else {
-    var node = this.head;
-    while (node) {
-      if (this.compare(value, node.data)) {
-        var prev = node.prev;
-        if (prev) {
-          prev.next = newNode;
-        }
-        node.prev = newNode;
-        newNode.prev = prev;
-        newNode.next = node;
-        if (this.head == node) {
-          this.head = newNode;
-        }
-        break;
-      }
-      node = node.next;
-    }
-    if (!node) {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
-    }
+    this.tail_.next = newNode;
+    newNode.prev = this.tail_;
+    this.tail_ = newNode;
   }
-}
+  this.size_++;
+};
 
 HashList.prototype.get = function(key) {
-  var node = this.map[key];
+  var node = this.map_[key];
   if (node) {
     return node.data;
   } else {
     return null;
   }
-}
+};
 
-HashList.prototype.del = function(key) {
-  var node = this.map[key];
+HashList.prototype.remove = function(key) {
+  var node = this.map_[key];
   if (node) {
     var prev = node.prev;
     var next = node.next;
@@ -72,65 +52,36 @@ HashList.prototype.del = function(key) {
     if (next) {
       next.prev = prev;
     }
-    delete this.map[key];
+    delete this.map_[key];
+    this.size_--;
   }
-}
+};
 
-HashList.prototype.select = function(offsetKey, limit, reverse) {
+HashList.prototype.select = function(offset, limit, reverse) {
   var result = [];
   if (limit <= 0) {
     return result;
   }
 
-  var node = this.map[offsetKey];
-  if (!node) {
+  var node = reverse ? this.tail_ : this.head_;
+  while (node && offset > 0) {
     if (reverse) {
-      node = this.tail;
+      node = node.prev;
     } else {
-      node = this.head;
+      node = node.next;
     }
+    offset--;
   }
-  while (node && limit--) {
+  while (node && limit > 0) {
     result.push(node.data);
     if (reverse) {
       node = node.prev;
     } else {
       node = node.next;
     }
+    limit--;
   }
   return result;
-}
+};
 
-HashList.prototype.print = function() {
-  var node = this.head;
-  while (node) {
-    console.log(node.data);
-    node = node.next;
-  }
-}
-
-if (!module.parent) {
-  var list = new HashList(function(left, right) {
-    return left.score >= right.score;
-  });
-  list.add('k1', {name: 'qingfeng', score: '1'});
-  list.add('k2', {name: 'qingfeng', score: '8'});
-  list.add('k3', {name: 'qingfeng', score: '4'});
-  list.add('k4', {name: 'qingfeng', score: '7'});
-  list.add('k5', {name: 'qingfeng', score: '2'});
-  list.add('k6', {name: 'qingfeng', score: '3'});
-  list.add('k7', {name: 'qingfeng', score: '5'});
-  list.add('k8', {name: 'qingfeng', score: '6'});
-
-  console.log('--------------------');
-  list.print();
-  console.log('--------------------');
-
-  console.log('get k1', list.get('k1'));
-  console.log('get k2', list.get('k2'));
-  console.log('select k2 4', list.select('k2', 4));
-  console.log('select k1 4 reverse', list.select('k1', 4, true));
-  list.del('k4');
-  console.log('after remove k4, select k2 4', list.select('k2', 4));
-  console.log('after remove k4, select k1 4 reverse', list.select('k1', 4, true));
-}
+module.exports = HashList;
---
+++ javascript/hash-list/test.js
@@ -0,0 +1,78 @@
+/*jshint expr: true*/
+/*global describe, it*/
+
+var should = require('should');
+var HashList = require('./hash-list');
+
+describe('hash-list', function() {
+  it('test empty list', function(done) {
+    var list = new HashList();
+    should(list.get()).not.exists;
+    should(list.get(1)).not.exists;
+    should(list.get('abc')).not.exists;
+    list.size().should.equal(0);
+    list.remove('abc');
+    list.size().should.equal(0);
+    var result1 = list.select(0, 10);
+    result1.should.eql([]);
+    var result2 = list.select(0, 10, true);
+    result2.should.eql([]);
+    list.size().should.equal(0);
+    done();
+  });
+
+  it('test normal operation', function(done) {
+    var list = new HashList();
+    list.size().should.equal(0);
+    var tom = {name: 'tom', age: 27};
+    list.add('tom', tom);
+    list.get('tom').should.eql(tom);
+    list.size().should.equal(1);
+    list.remove('not-exists-key');
+    list.size().should.equal(1);
+
+    list.select(0, 1).should.eql([tom]);
+    list.select(0, 10).should.eql([tom]);
+    list.select(1, 10).should.eql([]);
+
+    list.select(0, 1, true).should.eql([tom]);
+    list.select(0, 10).should.eql([tom]);
+    list.select(1, 10).should.eql([]);
+
+    var jack = {name: 'jack', age: 30};
+    list.add('jack', jack);
+    list.size().should.equal(2);
+    list.get('jack').should.eql(jack);
+
+    list.select(0, 1).should.eql([tom]);
+    list.select(0, 2).should.eql([tom, jack]);
+
+    list.select(0, 1, true).should.eql([jack]);
+    list.select(0, 2, true).should.eql([jack, tom]);
+
+    var lucy = {name: 'lucy', age: '18'};
+    list.add('lucy', lucy);
+    list.size().should.equal(3);
+    list.get('lucy').should.eql(lucy);
+
+    list.select(0, 3).should.eql([tom, jack, lucy]);
+    list.select(0, 4).should.eql([tom, jack, lucy]);
+    list.select(1, 4).should.eql([jack, lucy]);
+    list.select(2, 4).should.eql([lucy]);
+    list.select(3, 4).should.eql([]);
+
+    list.select(0, 3, true).should.eql([lucy, jack, tom]);
+    list.select(0, 4, true).should.eql([lucy, jack, tom]);
+    list.select(1, 4, true).should.eql([jack, tom]);
+    list.select(2, 4, true).should.eql([tom]);
+    list.select(3, 4, true).should.eql([]);
+
+    list.remove('jack');
+    list.size().should.equal(2);
+    should(list.get('jack')).not.exists;
+    list.select(0, 2).should.eql([tom, lucy]);
+    list.select(0, 2, true).should.eql([lucy, tom]);
+
+    done();
+  });
+});
