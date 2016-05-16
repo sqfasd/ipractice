b76c3bd6aa5a31223e743ee15aaa8f89466c0014
sqfasd <sqf1225@foxmail.com>
2016-05-16 13:17:23 +0800

    notify the user if download again

--- framework/express/hello/routes/index.js
+++ framework/express/hello/routes/index.js
@@ -7,15 +7,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var isDownloaded = false;
+
 router.get('/download', function(req, res, next) {
-  var content = '32804302409\r\nfdsjlkflkds\r\ndfsfdsafds';
-  var filename = 'asch-key.txt';
-  var mimetype = mime.lookup(filename);
-  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-  res.setHeader('Content-Length', content.length);
-  res.setHeader('Content-Type', mimetype);
-  res.write(content, 'binary');
-  res.end();
+  console.log('isDownloaded', isDownloaded);
+  if (!isDownloaded) {
+    var content = '32804302409\r\nfdsjlkflkds\r\ndfsfdsafds';
+    var filename = 'asch-key.txt';
+    var mimetype = mime.lookup(filename);
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.setHeader('Content-Length', content.length);
+    res.setHeader('Content-Type', mimetype);
+    res.write(content, 'binary');
+    res.end();
+    isDownloaded = true;
+  } else {
+    res.end('your key file has been downloaded!');
+  }
 });
 
 module.exports = router;
